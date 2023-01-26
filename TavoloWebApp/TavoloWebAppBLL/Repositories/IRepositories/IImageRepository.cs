a05de1bf87ef1197394ba79099c4dc135bdd1e73
Bartosz Sadowski <bshadowsky@gmail.com>
2023-01-26 19:37:49 +0100

    Image Repository added

---
+++ TavoloWebApp/TavoloWebAppBLL/Repositories/IRepositories/IImageRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TavoloWebAppBLL.Models;
+
+namespace TavoloWebAppBLL.Repositories.IRepositories
+{
+    public interface IImageRepository
+    {
+        public void Create(Image image);
+    }
+}
---
+++ TavoloWebApp/TavoloWebAppBLL/Repositories/ImageRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TavoloWebAppBLL.Data;
+using TavoloWebAppBLL.Models;
+using TavoloWebAppBLL.Repositories.IRepositories;
+
+namespace TavoloWebAppBLL.Repositories
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ImageRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Image image)
+        {
+            _context.Images.Add(image);
+            _context.SaveChanges();
+        }
+    }
+}
