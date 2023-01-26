957a45549c0c7d3897f691c222497ca0a4d46817
Bartosz Sadowski <bshadowsky@gmail.com>
2023-01-26 19:38:07 +0100

    Image Service Added

---
+++ TavoloWebApp/TavoloWebAppBLL/Services/IServices/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TavoloWebAppBLL.Models;
+
+namespace TavoloWebAppBLL.Services.IServices
+{
+    public interface IImageService
+    {
+        public void Create(Image image);
+    }
+}
---
+++ TavoloWebApp/TavoloWebAppBLL/Services/ImageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TavoloWebAppBLL.Models;
+using TavoloWebAppBLL.Repositories.IRepositories;
+using TavoloWebAppBLL.Services.IServices;
+
+namespace TavoloWebAppBLL.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly IImageRepository _imageRepository;
+
+        public ImageService(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
+
+        public void Create(Image image)
+        {
+            _imageRepository.Create(image);
+        }
+    }
+}
