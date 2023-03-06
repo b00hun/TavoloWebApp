dd4d5ad9168359309fd98ea448f748a3fbf912df
Bartosz Sadowski <bshadowsky@gmail.com>
2023-03-06 20:58:59 +0100

    Added Contact controller

---
+++ TavoloWebApp/TavoloWebApp/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TavoloWebAppASP.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendEmail()
+        {
+            return View();
+        }
+    }
+}
